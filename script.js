f9b652410ab735b2b4a67f7b8ccd009f40564c80
AnishSil-Bacancy <136692625+AnishSil-Bacancy@users.noreply.github.com>
2023-06-15 14:28:31 +0530

    Comments
    added comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const main = document.querySelector('#main')
+// get the main element under which we will have our posts
 
 function createPost({id, title, body }, userName) {
+    // Creates the post content
     const htmlSegment = `
     <div class="card m-2 bg-light" style="width: 30rem;">
         <div class="card-body">
@@ -25,13 +27,13 @@ function createPost({id, title, body }, userName) {
         </div>
         </div>
     `
-    
+    // add each individual post to the main 
     main.innerHTML+=htmlSegment
 }
 
 function createComments({email, body, postId}){
+    // create comments for each post
     const comment = document.querySelector(`#comment-box-${postId}`)
-    const html = comment.innerHTML
     const htmlSegment = 
     `  <li class="list-group-item d-flex justify-content-between align-items-start">
             <div class="ms-2 me-auto">
@@ -40,20 +42,24 @@ function createComments({email, body, postId}){
             </div>
         </li>
     `
+    // add each individual comment under the post
     comment.innerHTML += htmlSegment
 }
 
 async function getUsers() {
+    // gets us the username to map with userId
     let url = `https://jsonplaceholder.typicode.com/users`
     let userData = await fetch(url)
     return userData.json()
 }
 
 async function getPost(user) {
+    // gets the data for each post
     let url = `https://jsonplaceholder.typicode.com/posts`
     let postData = await fetch(url)
     let post = await postData.json() 
     
+    //loop through each postsdata and create the post
     post.forEach(element => {
         let userName = user[element.userId -1].username
         createPost(element,userName)
@@ -61,6 +67,7 @@ async function getPost(user) {
 }
 
 async function getComments() {
+    // gets the comments and create comments
     let url = `https://jsonplaceholder.typicode.com/comments`
     let commentData = await fetch(url)
     let comments = await commentData.json()
@@ -70,9 +77,11 @@ async function getComments() {
 }
 
 async function posts() {
+    // gets all of the functions together
     let user = await getUsers()
     await getPost(user)
     await getComments()
 }
 
+// calling the name function 
 posts()
